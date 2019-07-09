30cd5cc9d70dc716b2c0dbef265481539e05b9bc
Naseem Ali <naseem.ali.925@gmail.com>
2019-07-09 23:10:28 +0530

    Added Linter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 import "./App.css";
+import ButtonAppBar from "./components/ButtonAppBar";
+import ImageAvatars from "./components/ImageAvatars";
+import PersonalInfo from "./components/PersonalInfo";
 
 class App extends React.Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <ButtonAppBar />
+        <ImageAvatars />
+        <PersonalInfo />
+      </div>
+    );
   }
 }
 
---
+++ src/components/ImageAvatars.js
@@ -0,0 +1,31 @@
+// import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+// import Avatar from '@material-ui/core/Avatar';
+// import Grid from '@material-ui/core/Grid';
+import Naseem from '../static/images/naseem.png';
+
+const useStyles = makeStyles({
+  avatar: {
+    margin: 16,
+  },
+  bigAvatar: {
+    margin: 10,
+    width: 100,
+    height: 100,
+  },
+});
+
+export default function ImageAvatars() {
+  const classes = useStyles();
+
+  return (
+    <Grid container justify="center" alignItems="center">
+      {/* <Avatar
+        alt="Remy Sharp"
+        src="/static/images/avatar/1.jpg"
+        className={classes.avatar}
+      /> */}
+      <Avatar alt="Remy Sharp" src={Naseem} className={classes.bigAvatar} />
+    </Grid>
+  );
+}
