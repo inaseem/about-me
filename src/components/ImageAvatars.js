414bb8f26a1abe524d78f745a52e93c0f3cb7943
Naseem Ali <naseem.ali.925@gmail.com>
2019-07-09 23:47:31 +0530

    Changed Method

--- src/components/ImageAvatars.js
+++ src/components/ImageAvatars.js
@@ -1,7 +1,7 @@
-// import React from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-// import Avatar from '@material-ui/core/Avatar';
-// import Grid from '@material-ui/core/Grid';
+import Avatar from '@material-ui/core/Avatar';
+import Grid from '@material-ui/core/Grid';
 import Naseem from '../static/images/naseem.png';
 
 const useStyles = makeStyles({
--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -1,11 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 // import Button from '@material-ui/core/Button';
 // import Typography from '@material-ui/core/Typography';
-import Card from '@material-ui/core/Card';
+import Card from "@material-ui/core/Card";
 // import CardActions from '@material-ui/core/CardActions';
-// import CardContent from '@material-ui/core/CardContent';
-import { makeStyles } from '@material-ui/core/styles';
-import axios from 'axios';
+import CardContent from "@material-ui/core/CardContent";
+import { makeStyles } from "@material-ui/core/styles";
+import axios from "axios";
 
 const TOKEN = process.env.REACT_APP_TOKEN;
 console.log(`TOKEN =${TOKEN}`);
@@ -13,31 +13,31 @@ console.log(`TOKEN =${TOKEN}`);
 const useStyles = makeStyles({
   card: {
     minWidth: 275,
-    margin: 16,
+    margin: 16
   },
   bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)"
   },
   title: {
-    fontSize: 14,
+    fontSize: 14
   },
   pos: {
-    marginBottom: 12,
-  },
+    marginBottom: 12
+  }
 });
 export default function PersonalInfo() {
   const classes = useStyles();
   const [data, setData] = React.useState([]);
   useEffect(() => {
     const fetchData = async () => {
-      const response = await axios.get(
-        'https://api.github.com/user/repos',
+      const response = await axios.post(
+        "https://api.github.com/user/repos",
         {},
         {
-          Authorization: `token ${TOKEN}`,
-        },
+          headers: { Authorization: `token ${TOKEN}` }
+        }
       );
       console.log(`Response = ${response.data}`);
       setData(response.data);
