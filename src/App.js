687be3a06ce77ecd7542b717eb676c0636b3321d
Naseem Ali <naseem.ali.925@gmail.com>
2019-07-17 19:59:45 +0530

    Replaced BrowserHistory With HashHistory

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import "./App.css";
 import ButtonAppBar from "./components/ButtonAppBar";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 library.add(fab);
 class App extends React.Component {
   render() {
     return (
-      <Router>
+      <HashRouter basename="/">
         <ButtonAppBar />
-      </Router>
+      </HashRouter>
     );
   }
 }
